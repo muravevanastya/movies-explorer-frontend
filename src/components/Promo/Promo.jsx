56f79bde7719cf51f930823fe1618db9aadd2cfa
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-18 16:11:22 +0300

    добавила заголовок и подзаголовок

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,5 +1,11 @@
 function Promo() {
-
+  return (
+    <section className="promo">
+      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+      
+    </section>
+  )
 }
 
 export default Promo;
