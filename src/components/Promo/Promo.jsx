caf04d3159e1c6668dc7d482f0be6b5468d81f33
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-18 16:15:27 +0300

    добавила картинку и кнопку "узнать больше"

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,9 +1,14 @@
+import planet from '../../images/planet-web.svg';
+
 function Promo() {
   return (
     <section className="promo">
       <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
       <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-      
+      <img className='promo__picture' src={planet} alt="Планета" />
+      <div className='promo__more'>
+        <button className='promo__more-button'>Узнать больше</button>
+      </div>
     </section>
   )
 }
