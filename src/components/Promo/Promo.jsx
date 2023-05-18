16f0fc92bd471b7ab8948b40b8259b96a3ec1723
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-18 19:56:55 +0300

    расположила элементы

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -3,11 +3,15 @@ import planet from '../../images/planet-web.svg';
 
 function Promo() {
   return (
-    <section className="promo">
-      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-      <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-      <img className='promo__picture' src={planet} alt="Планета" />
-      <button className='promo__more-button'>Узнать больше</button>
+    <section className='promo'>
+      <div className='promo__container'>
+        <div className='promo__description'>
+          <h1 className='promo__title'>Учебный проект студента факультета <p>Веб-разработки.</p></h1>
+          <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <button className='promo__more-button'>Узнать больше</button>
+        </div>
+        <img className='promo__picture' src={planet} alt='Планета' />
+      </div>
     </section>
   )
 }
