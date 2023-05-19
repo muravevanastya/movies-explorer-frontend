119509c983931ca49f87b36a9585cd27bf9f13e0
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-19 16:58:29 +0300

    сверстала блок "о себе"

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,24 @@
+import './AboutMe.css';
+import studentPhoto from '../../images/photo-student.jpg'
+
+function AboutMe() {
+  return (
+    <section className='about-me'>
+      <h2 className='about-me__title'>Студент</h2>
+      <div className='about-me__container'>
+        <div className='about-me__description'>
+          <h3 className='about-me__name'>Анастасия</h3>
+          <p className='about-me__job'>Фронтенд-разработчик, 20 лет</p>
+          <p className='about-me__text'>Я родилась в Калининграде, жила в Санкт-Петербурге, училась в Нидерландах, а полгода назад переехала на Кипр.
+          Променяла карьеру экономиста на путь программиста. Год назад начала проходить курсы по веб-разработке от Яндекс Практикума и с тех пор развиваюсь
+          в этой сфере: участвую в проектах со своими единомышленниками, создаю собственные проекты для пополнения портфолио, решаю алгоритмические задачи,
+          читаю дополнительную литературу. Моя цель - не стабильность, а постоянное развитие своих навыков.</p>
+          <p className='about-me__github'>Github</p>
+        </div>
+        <img className='about-me__photo' src={studentPhoto} alt="Фото Анастасии" />
+      </div>
+    </section>
+  )
+}
+
+export default AboutMe;
