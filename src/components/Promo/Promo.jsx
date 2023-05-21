3cdaed2096a78f87f4a164ee6f6f38534693f1c5
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-21 14:45:45 +0300

    сделала адаптив для главной страницы

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -6,7 +6,7 @@ function Promo() {
     <section className='promo'>
       <div className='promo__container'>
         <div className='promo__description'>
-          <h1 className='promo__title'>Учебный проект студента факультета <p>Веб-разработки.</p></h1>
+          <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
           <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
           <button className='promo__more-button'>Узнать больше</button>
         </div>
