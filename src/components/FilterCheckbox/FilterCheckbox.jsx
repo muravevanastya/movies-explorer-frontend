b6a5ec914a19790194aa7856339a0068ac89ec86
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-26 21:43:30 +0300

    сделала адаптив для страницы с фильмами

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -2,12 +2,14 @@ import './FilterCheckbox.css';
 
 function FilterCheckbox() {
   return (
-    <div className='filter'>
-      <label className='filter__switcher'>
-        <input type="checkbox" className='filter__checkbox' />
-        <span className='filter__checkbox-slider'></span>
-      </label>
-      <p className='filter__switcher-text'>Короткометражки</p>
+    <div className='search-form__filter'>
+      <div className='search-form__filter-items'>
+        <label className='filter__switcher'>
+          <input type="checkbox" className='filter__checkbox' />
+          <span className='filter__checkbox-slider'></span>
+        </label>
+        <p className='filter__switcher-text'>Короткометражки</p>
+      </div>
     </div>
   )
 }
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,18 +1,17 @@
 import './SearchForm.css';
-import searchIcon from '../../images/search-icon.svg';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 function SearchForm() {
   return (
-    <form className='search-form'>
+    <form className='movies__search-form'>
       <div className='search-form__container'>
         <div className='search-form__input-container'>
-          <img className='search-form__icon' src={searchIcon} alt='Лупа' />
+          <div className='search-form__icon'></div>
           <input type='text' className='search-form__input' placeholder='Фильм'/>
         </div>
         <button className='search-form__button'>Найти</button>
-        <FilterCheckbox />
       </div>
+      <FilterCheckbox />
     </form>
   )
 }
