c6139444a6732b20ee5ce335b871189794b0fa82
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-29 13:12:28 +0300

    сверстала страницу с авторизацией

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
+import Login from '../Login/Login';
 
 function App() {
   return (
@@ -22,7 +23,9 @@ function App() {
         <Route path='/profile'
           element={<Profile />}
         />
-        <Route path='/signin'/>
+        <Route path='/signin'
+          element={<Login />}
+        />
         <Route path='/signup'
           element={<Register />}
         />
