32b4acbbc8798b11ce7eb06c608a77dd9b8e3f96
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-29 17:36:17 +0300

    подправила адаптив

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,15 +1,11 @@
 import Movies from "../Movies/Movies";
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 
 function SavedMovies() {
   return (
     <>
-    <Header/>
     <main className='saved-movies'>
       <Movies />
     </main>
-    <Footer/>
     </>
   )
 }
