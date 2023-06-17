0a574a9c0d3492045e37be8d975fbbd63bf1c70d
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-06-17 14:25:01 +0300

    реализовала вычисление длительности фильма

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,13 +1,25 @@
 import './MoviesCard.css';
 
 function MoviesCard({movie}) {
+
+  function getMoviesDuration() {
+    const hour = Math.floor((movie.duration / 60)) + 'ч';
+    
+    if(movie.duration % 60 === 0) {
+      return hour;
+    } else if(movie.duration > 60) {
+      return hour + ' ' + (movie.duration % 60 + 'м');
+    } else {
+      return movie.duration + 'м';
+    }
+  }
   
   return (
     <div className='movie'>
       <div className='movie__description'>
         <div className='movie__info'>
           <p className='movie__name'>{movie.nameRU}</p>
-          <p className='movie__duration'>1ч 42м</p>
+          <p className='movie__duration'>{getMoviesDuration()}</p>
         </div>
         <button className='movie__save'></button>
       </div>
